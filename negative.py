e797d0dd9a6a3af4a61b6e0d3444dce1cfb107c3
fawwazmayda <maydafawwaz@gmail.com>
2019-08-28 09:05:07 -0700

    menambah argument parser

--- negative.py
+++ negative.py
@@ -3,8 +3,10 @@ import argparse
 import numpy as np 
 
 
+parser = argparse.ArgumentParser(description='Process some image.')
+parser.add_argument('--file',required=True,type=str)
 #img1 and img2 must be in same size
-img1 = cv2.imread('download.jpg', 1)
+img1 = cv2.imread(args.file, 1)
 img1 = cv2.cvtColor(img1,cv2.COLOR_BGR2RGB)
 img2 = (255 -  np.array(img1))
 
