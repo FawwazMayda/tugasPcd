ea6137572c4c350070f54517b65c4fd12b271d29
fawwazmayda <maydafawwaz@gmail.com>
2019-08-28 08:57:29 -0700

    Menambah fungsi gamma dan waitkey

--- gamma.py
+++ gamma.py
@@ -1,2 +1,19 @@
 import cv2
 import numpy as np 
+
+
+def scaled(img,gamma=1.0):
+    img = np.array(img)
+    invGamma  = 1 / gamma
+    img_scaled = img/255
+    img_gamma = img_scaled ** invGamma
+    img_res = img_gamma * 255
+    return img_res
+
+
+#img1 and img2 must be in same size
+img1 = cv2.imread('download.jpg', 1)
+img2 = scaled(img1)
+
+cv2.imshow('img1',img1)
+cv2.imshow('img2',img2)
