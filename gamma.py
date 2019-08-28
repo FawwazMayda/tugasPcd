e6bcafb1064314c0d538ff5b25af658a679eb44e
fawwazmayda <maydafawwaz@gmail.com>
2019-08-28 09:04:35 -0700

    menambah argument parser

--- gamma.py
+++ gamma.py
@@ -1,8 +1,16 @@
 import cv2
 import numpy as np 
+import argparse
+
+parser = argparse.ArgumentParser(description='Process some image.')
+parser.add_argument('--gamma',required=True,type=float)
+parser.add_argument('--file',required=True,type=str)
 
 
-def scaled(img,gamma=1.0):
+
+args = parser.parse_args()
+print(args)
+def scaled(img,gamma=0.2):
     img = np.array(img)
     invGamma  = 1 / gamma
     img_scaled = img/255
@@ -12,8 +20,11 @@ def scaled(img,gamma=1.0):
 
 
 #img1 and img2 must be in same size
-img1 = cv2.imread('download.jpg', 1)
-img2 = scaled(img1)
+img1 = cv2.imread(args.file, 1)
+img2 = scaled(img1,float(args.gamma))
 
 cv2.imshow('img1',img1)
 cv2.imshow('img2',img2)
+
+cv2.waitKey(0)
+cv2.destroyAllWindow()
